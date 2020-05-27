c539c5b6e09dc7014eb74dbd5b1a5361d2cf052d
lukajvnv <44602021+lukajvnv@users.noreply.github.com>
2020-05-27 13:48:18 +0200

    Add datatables to entities overview page (#33)

--- utilities/template_utils.py
+++ utilities/template_utils.py
@@ -45,7 +45,10 @@ def get_application_name(configs_field):
 def get_jsp_metadata():
     bootstrap_css = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
     bootstrap_js = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
-    return bootstrap_css, bootstrap_js
+    jquery = 'jquery-3.5.1.min.js'
+    datatables_css = 'https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css'
+    datatables_js = 'https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js'
+    return bootstrap_css, bootstrap_js, jquery, datatables_css, datatables_js
 
 
 def write_to_file(content, file_name):
